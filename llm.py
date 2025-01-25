4ec673bb11489ca1dc6a5259c0831cca54cb340f
Yashraj67 <www.yindustries@gmail.com>
2025-01-25 05:50:32 +0530

    Rag based answering model

---
+++ llm.py
@@ -0,0 +1,38 @@
+from openai import OpenAI
+
+client = OpenAI(
+    base_url="https://integrate.api.nvidia.com/v1",
+    api_key="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",
+)
+
+
+def get_llm_response(question, docs):
+    completion = client.chat.completions.create(
+        model="meta/llama-3.1-405b-instruct",
+        messages=[
+            {
+                "role": "user",
+                "content": f"""
+                    Given the following context, 
+                    if relevant use it to answer the user qustion else use your own knowledge to answer.
+                    
+                    <context>
+                        {docs}
+                    <context>
+
+                    <user_query>
+                        {question}
+                    <user_query>
+                 """,
+            }
+        ],
+        temperature=0.2,
+        top_p=0.7,
+        stream=True,
+    )
+    response = ""
+    for chunk in completion:
+        if chunk.choices[0].delta.content is not None:
+            response += chunk.choices[0].delta.content
+
+    return response
